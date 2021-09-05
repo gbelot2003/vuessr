72632615543c12e6cf8ae6237180b9916f817fd6
Gerardo Belot <gbelot2003@hotmail.com>
2021-09-04 22:59:11 -0600

    :rocket:  Adding Vuejs and other necesary systems

--- web/resources/js/app.js
+++ web/resources/js/app.js
@@ -1,1 +1,13 @@
-require('./bootstrap');
+
+import Vue from 'vue';
+
+import vuetify from './vuetify';
+
+import App from './components/App';
+
+
+export default new Vue({
+    el: '#app',
+    vuetify,
+    render: h => h(App)
+});
---
+++ web/resources/js/components/ExampleComponent.vue
@@ -0,0 +1,17 @@
+<template>
+  <v-app app>
+    <v-container>
+      <v-btn color="primary"> Primary </v-btn>
+      <v-btn color="secondary"> Secondary </v-btn>
+      <v-btn color="error"> Error </v-btn>
+    </v-container>
+  </v-app>
+</template>
+
+<script>
+export default {
+  mounted() {
+    console.log("Component mounted.");
+  },
+};
+</script>
